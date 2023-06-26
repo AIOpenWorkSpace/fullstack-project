d2ffb4813f8a15a5338aaa716148b4c1a0dde714
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-26 15:23:21 -0400

    add routes and scaffold

---
+++ src/components/BestMovies.js
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import axios from 'axios';
+
+// class BestMovies extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movies: [],
+      
+//     };
+//   }
+
+//   getMovies = async () => {
+//     try {
+//       let url = `${process.env.REACT_APP_SERVER}/movies`;
+//       let MoviesFromDB = await axios.get(url);
+
+//       this.setState({
+//         books: MoviesFromDB.data
+//       });
+//     } catch (error) {
+//       console.log(error.message);
+//     }
+//   };
+
+
+
+
+//   render() {
+//     return (
+//       <>
+  
+//       </>
+//     );
+//   }
+// }
+
+// export default BestMovies;
