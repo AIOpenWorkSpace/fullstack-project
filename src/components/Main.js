42b37988f733fcfcfd583a818058a3c04c4728f1
Jennifer Sung <sung.jenni93@gmail.com>
2023-06-26 15:44:51 -0400

    rename main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 // import React from 'react';
 // import axios from 'axios';
 
-// class BestMovies extends React.Component {
+// class Main extends React.Component {
 //   constructor(props) {
 //     super(props);
 //     this.state = {
@@ -35,4 +35,4 @@
 //   }
 // }
 
-// export default BestMovies;
+// export default Main;
