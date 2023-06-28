0dfd490a87d7bf4adceb2bb58daf81b7dab8f5f7
Aaron Clark <amclarkwustl@gmail.com>
2023-06-28 11:20:28 -0700

    added login page styling

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { FaFilm } from 'react-icons/fa';
+import './style/LoginButton.css';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <div className="login-page">
+      <div className="login-box">
+        <h1><FaFilm></FaFilm> Welcome to CineWatch AI</h1>
+        <p>Please Log In To Continue</p>
+        <button className="login-button" onClick={() => loginWithRedirect()}>Log In</button>
+      </div>
+      
+    </div>
+  )
 };
 
 export default LoginButton;
-
-/**
- * all decor related things to the button occur in here
- */
