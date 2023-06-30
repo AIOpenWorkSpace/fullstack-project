a1bced1617c4ac844983f5171e3f367187f53ab9
Aaron Clark <amclarkwustl@gmail.com>
2023-06-30 12:10:34 -0700

    more spinner updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -35,14 +35,21 @@ class Main extends React.Component {
     this.setState({ spoilers: event.target.checked });
   };
 
+  componentDidUpdate(prevState) {
+    if (prevState.renderSpinner !== this.state.renderSpinner) {
+      this.forceUpdate();
+    }
+  }
+
   handleSearchSubmit = async (event) => {
     event.preventDefault();
     let spoilers = this.state.spoilers ? ('Please avoid any spoilers.') : ('');
     let prompt = `Evaluate "${this.state.searchQuery}" on all of these categories for people of ${this.state.ageRange} : language usage, alcohol and other drugs, portrayal of sex and romantic relationships, positive role models, positive messages, diverse representation, violence, product placement.  Please provide a full response for each category even if the entire movie or show is not appropriate for viewers of this age.  ${spoilers}`;
-    await this.setState({renderSpinner: true})
+    this.setState({renderSpinner: true})
 
     try {      
       console.log(this.state.renderSpinner);
+      
       let updatedMovieFromAxios = await axios.post(
         `${process.env.REACT_APP_SERVER}/ask/${this.state.searchQuery}`,
         { prompt }
